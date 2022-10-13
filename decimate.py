a36ca73a8ad8c0837ca1d924d3e4a866c55f2782
Clément Broutin <clement.zogloglo@gmail.com>
2022-10-13 10:52:22 +0200

    decimation avec d=0.9*dm

--- decimate.py
+++ decimate.py
@@ -13,6 +13,52 @@ class Decimater(obja.Model):
         super().__init__()
         self.deleted_faces = set()
 
+    def decimaton(self):
+        distances = []
+        index_vertices = []
+        voisins_vertices = []
+        for (vertex_index, vertex) in enumerate(self.vertices):
+            liste_points = []
+            for (face_index, face) in enumerate(self.faces):
+                L = [face.a, face.b, face.c]
+                if vertex_index in L:
+                    for l in L:
+                        if l not in liste_points+[vertex_index]:
+                            liste_points.append(l)
+            if len(liste_points) > 4:
+                A = np.ones(len(liste_points)+1, 4)
+                A[0, 0:3] = vertex
+                for (i, l) in enumerate(liste_points):
+                    A[i+1, 0:3] = self.vertices[l]
+                tAA = A.transpose*A
+                lambdas, vecteurs_propres = np.linalg.eig(tAA)
+                ind = np.argmin(lambdas)
+                X = vecteurs_propres[:, ind]
+                n = np.array(X[0:3])
+                k = (-X[3]-n.transpose*vertex)/np.linalg.norm(vertex)
+                distances.append(abs(k))
+                index_vertices.append(vertex_index)
+                voisins_vertices.append(liste_points[:])
+
+
+
+        indices_tries = [i[0] for i in sorted(enumerate(distances), key=lambda x:x[1], reverse=True)]
+        d0 = 0.9*distances[indices_tries[0]]
+        dont_touch = []
+        i = 0
+        deleted_vertices = []
+        while distances[indices_tries[i]] > d0 and i < len(indices_tries):
+            if index_vertices[indices_tries[i]] not in dont_touch:
+                deleted_vertices.append(index_vertices[indices_tries[i]])
+                dont_touch.append(voisins_vertices[indices_tries[i]])
+            i += 1
+        if len(tampon) < len(self.vertices):
+            return deleted_vertices
+        else:
+            return []
+
+
+
     def contract(self, output):
         """
         Decimates the model stupidly, and write the resulting obja in output.
