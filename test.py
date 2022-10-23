68ddb8d8ef96fc073c4c26191b682831f54d22ff
Christophe Pantel <christophe.pantel@etu.toulouse-inp.fr>
2022-10-23 18:04:21 +0200

    Ajout commentaires.

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ class Test(obja.Model):
     """
     A test class that decimates a 3D model with arbitrary triangle meshes.
     """
-        
+
+    # Facettes TODO CP
     faces_rec = []
         
     def __init__(self):
@@ -25,13 +26,19 @@ class Test(obja.Model):
         """
         # sommets = [6] # sommet 7 du mesh test.py
         # sommets = [75] # patch 76 317 355 303 354 316 pour suzanne
+        # Nombre de sommets
         nb_vert_init = len(self.vertices)
+        # TODO CP
         op = []
+        # numéro de l'itération
         tour = 1
         
         # while len(self.vertices)/nb_vert_init >= 0.1:
+        # tant que le nombre d'itération est inférieur à 3
+        # CP : y a t'il une raison particulière pour 3 ?
         while tour < 3:
             print("tour n° " + str(tour) + "\n")
+            # Choix des sommets qui peuvent être supprimés
             sommets = decimation(self)
             (patch, faces) = self.find_patches(sommets)
             op = trace_Z(self, patch, faces, sommets, op)
@@ -59,6 +66,7 @@ class Test(obja.Model):
             # print(any(x is None for x in self.vertices))
             # print("Y a t il des None dans self.faces ? ")
             # print(any(x is None for x in self.faces))
+            # Passage à l'itération suivante
             tour += 1
             
                     
@@ -92,10 +100,11 @@ class Test(obja.Model):
         
     def sort_patch_rec(self, list_faces, del_vert, vert_prec):
         """
-        Fonction récursive qui range dans les faces d'un patch, avant suppression du
+        Fonction récursive qui range dans les facettes d'un patch, avant suppression du
         sommet au centre, et retourne la liste des indices des sommets du patch dans 
         un ordre de contour.
-        (L'ordre se décide lors de l'appel de la fonction avec le paramètre vert_prec)
+        (L'ordre se décide lors de l'appel de la fonction avec le paramètre vert_prec qui est le précédent du
+        sommet à supprimer dans le contour)
         """
         # print('list_faces : '+str(list_faces))
         # print('del_vert : '+str(del_vert))
@@ -105,13 +114,14 @@ class Test(obja.Model):
         else:
             # Pour chaque facette
             for ind in range(len(list_faces)):
-                (f,_) = list_faces[ind]
+                # facette courante
+                ( f, _) = list_faces[ind]
                 # liste des sommets de la facette courante
                 verts_f = [f.a, f.b, f.c]
                 # print(verts_f)
                 # print('--------------------------------------')
-                # si le sommet à supprimer fait parti de la facette courante
-                # et le sommet précédent fait aussi parti de la facette courante
+                # si le sommet à supprimer fait partie de la facette courante
+                # et le sommet précédent fait aussi partie de la facette courante
                 if del_vert in verts_f and vert_prec in verts_f:
                     # Pour chaque sommet de la facette courante
                     for v in verts_f:
@@ -186,25 +196,34 @@ class Test(obja.Model):
 
 def trace_Z(self, patches, faces_in_the_patches, vertices_to_delete, operations):
     """
-    Fonction qui effectue les modification sur le mesh. Plus précisément supprime les
-    points des milieus de patchs et trace les Z.
+    Fonction qui modifie le maillage : supprime les sommets du milieu de patchs et trace les Z.
     
-    arguments : - liste des patchs (liste de liste d'indices de sommets de contour)
-                - liste des faces dans les patchs (liste de liste de tuples (face,indice) )
+    Paramètres : - liste des patchs (liste de liste d'indices des sommets du contour de chaque patch)
+                - liste des facettes dans les patchs (liste de liste de tuples (facette,indice) )
                 - liste des sommets au milieu des patchs à supprimer (liste d'indices de sommets)
     
-    retour :    - liste des opérations à effectuer (list de string)   
-    """   
+    Retour : - liste des opérations à effectuer (list de string)
+    """
+    # Pour chaque indice de patch
     for i in range(len(patches)):
+        # Patch courant : suite de sommets qui forment le contour du patch
         patch = patches[i]
-        #supprimer deux faces en face l'une de l'autre
+        # suppression des 2 facettes opposées (numéro 0 et n/2)
+        # il y en a au moins 2 car les patchs contiennent au moins
         n2 = math.ceil(len(faces_in_the_patches[i])/2)
+        # Ajout de l'opération que TODO
         operations.append(('af', 0, faces_in_the_patches[i][0][0]))
+        # Indices des facettes à supprimer
         ind_to_del = self.faces.index(faces_in_the_patches[i][0][0])
+        # Suppression des facettes concernées
         self.faces[ind_to_del] = None
+        # Ajout de l'opération que TODO
         operations.append(('af', n2, faces_in_the_patches[i][n2][0]))
+        # Indices des facettes à supprimer
         ind_to_del = self.faces.index(faces_in_the_patches[i][n2][0])
+        # Suppression des facettes concernées
         self.faces[ind_to_del] = None
+        # Sommet à supprimer
         vert_del = vertices_to_delete[i]        
         
         # print('taille du patch : '+str(len(faces_in_the_patches[i])))
@@ -217,41 +236,74 @@ def trace_Z(self, patches, faces_in_the_patches, vertices_to_delete, operations)
         # print(vertices_to_delete)
         # print()
 
+        # Pour chaque indice de facette dans le patch courant
         for j in range(len(faces_in_the_patches[i])):
+            # facette courante et son indice dans le patch (TODO CP : c'est à dire)
             (face, index) = faces_in_the_patches[i][j]
+            # si la facette de l'indice courant n'est pas vide (TODO CP : c'est à dire)
+            # ce n'est pas une facette qui a déjà été supprimée
             if self.faces[index] is not None:                   ########## ICI J'AI CHANGE != PAR is not
-                # modifier les n-2 faces restantes
+                # modifier les n - 2 facettes restantes du patch courant
+
+                # si le sommet à supprimer est le sommet a de la facette
                 if vert_del == face.a:
-                    
-                    if patch.index(face.b) == 0 :
+                    # si la position dans le contour du patch du sommet b de la facette courante est 0 (TODO CP : c'est à dire)
+                    # il s'agit d'une facette adjacente à la facette 0 du patch
+                    if patch.index(face.b) == 0:
+                        # création d'une nouvelle facette à partir des sommets b, c et du
+                        # sommet obtenu dans le contour de la facette à partir de l'indice
+                        # de c dans le contour
                         new_face = obja.Face(face.b, face.c, patch[-patch.index(face.c)])
+                    # si la position dans le patch du sommet b de la facette courante est n / 2 (TODO CP : c'est à dire)
+                    # il s'agit d'une facette adjacente à la facette n / 2 du patch
                     elif patch.index(face.b) == n2:
+                        # création d'une nouvelle facette à partir des sommets b, c et du
+                        # sommet obtenu dans le contour de la facette à partir de l'indice
+                        # de c dans le contour
                         new_face = obja.Face(face.b, face.c, patch[-patch.index(face.c)])
+                    # sinon, c'est une facette entre 0 et n / 2 qui n'est pas adjacente
                     else:
+                        # création d'une nouvelle facette à partir des sommets b, c et du
+                        # sommet obtenu dans le contour de la facette à partir de l'indice
+                        # de b dans le contour
                         new_face = obja.Face(face.b, face.c, patch[-patch.index(face.b)])
-                        
+
+                # si le sommet à supprimer est le sommet b de la face
                 elif vert_del == face.b:
-                    
-                    if patch.index(face.c) == 0 :
+                    # si la position dans le patch du sommet c de la facette courante est 0 (TODO CP : c'est à dire)
+                    if patch.index(face.c) == 0:
+                        # création d'une nouvelle facette à partir des sommets c, a et de TODO CP
                         new_face = obja.Face(face.c, face.a, patch[-patch.index(face.a)])
+                    # si la position dans le patch du sommet c de la facette courante est n / 2 (TODO CP : c'est à dire)
                     elif patch.index(face.c) == n2:
+                        # création d'une nouvelle facette à partir des sommets b, c et de TODO CP
                         new_face = obja.Face(face.c, face.a, patch[-patch.index(face.a)])
+                    #
                     else:
+                        # création d'une nouvelle facette à partir des sommets c, a et de TODO CP
                         new_face = obja.Face(face.c, face.a, patch[-patch.index(face.c)])
-                        
+
+                # si le sommet à supprimer est le sommet c de la face
                 elif vert_del == face.c:
-                    
-                    if patch.index(face.a) == 0 :
+                    # si la position dans le patch du sommet a de la facette courante est 0 (TODO CP : c'est à dire)
+                    if patch.index(face.a) == 0:
+                        # création d'une nouvelle facette à partir des sommets a, b et de TODO CP
                         new_face = obja.Face(face.a, face.b, patch[-patch.index(face.b)])
+                    # si la position dans le patch du sommet a de la facette courante est n / 2 (TODO CP : c'est à dire)
                     elif patch.index(face.a) == n2:
+                        # création d'une nouvelle facette à partir des sommets a, b et de TODO CP
                         new_face = obja.Face(face.a, face.b, patch[-patch.index(face.b)])
+                    #
                     else:
+                        # création d'une nouvelle facette à partir des sommets a, b et de TODO CP
                         new_face = obja.Face(face.a, face.b, patch[-patch.index(face.a)])
-                        
+
+                # sinon : Ce cas n'est pas possible
                 else:
                     print("ERREUR !!")
                     print(vert_del)
-                    
+
+                # Ajout de l'opération que effacer la face à l'indice
                 operations.append(('ef', index, face))
                 self.faces[index] = new_face
                             
@@ -307,6 +359,7 @@ def decimation(self):
         # Parcours de l'ensemble des sommets par indice (vertex_index) et valeur du sommet (vertex)
         for (vertex_index, vertex) in enumerate(self.vertices):
 
+            # Si le sommet courant existe
             if vertex is not None:
                 # Définition de la liste des points formant le patch 
                 liste_points = []
@@ -314,6 +367,7 @@ def decimation(self):
                 # Parcours de l'ensemble des facettes par indice (face_index) et valeur de la facette (face)
                 for (face_index, face) in enumerate(self.faces):
 
+                    # Si la facette existe
                     if face is not None:
                         # Liste des points d'une facette
                         L = [face.a, face.b, face.c]
@@ -324,13 +378,16 @@ def decimation(self):
                             # Parcours des sommets d'une facette
                             for l in L:
 
-                                # Si le sommet courant n'appartient pas à la liste des points d'un patch 
+                                # Si le sommet de la facette n'appartient pas à la liste des points d'un patch
+                                # et n'est pas le comme courant
                                 if l not in liste_points+[vertex_index]:
                                     # on ajoute le point à la liste de points d'un patch
                                     liste_points.append(l)
                                 # fin sommet courant n'appartient pas dans les points d'un patch
                             # fin parcours des sommets d'une facette
-                    # fin de sommet courant fait parti de la facette
+                        # fin de sommet courant fait parti de la facette
+                    # fin facette existe
+                # fin parcours facettes
 
                 # Si le nombre de points d'un patch est supérieur ou égal à 5 (on ne travaille pas sur des patchs de taille inférieure
                 # à 5)
@@ -366,6 +423,7 @@ def decimation(self):
                     # Mémorise les voisins du somme courant
                     voisins_vertices.append(liste_points[:])
                 # fin len(liste_points) >= 5
+            # fin sommet existe
         # fin du parcours des sommets
 
 
