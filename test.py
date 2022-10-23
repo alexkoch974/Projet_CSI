bc5527cbbc0a07269a98265ee36d702386674f0f
Alexandre Koch <alexandrekoch3@gmail.com>
2022-10-23 15:22:09 +0200

    dernieres petites erreurs

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ class Test(obja.Model):
     """
     A test class that decimates a 3D model with arbitrary triangle meshes.
     """
-    
+        
+    faces_rec = []
+        
     def __init__(self):
         super().__init__()
         
@@ -25,20 +27,39 @@ class Test(obja.Model):
         # sommets = [75] # patch 76 317 355 303 354 316 pour suzanne
         nb_vert_init = len(self.vertices)
         op = []
+        tour = 1
         
-        while len(self.vertices)/nb_vert_init >= 0.1:
+        # while len(self.vertices)/nb_vert_init >= 0.1:
+        while tour < 3:
+            print("tour n° " + str(tour) + "\n")
             sommets = decimation(self)
             (patch, faces) = self.find_patches(sommets)
-            print(self.faces[413])
             op = trace_Z(self, patch, faces, sommets, op)
             output_model = obja.Output(output, random_color=True)
             
-            for (index, face) in enumerate(self.faces):
-                if face is None:
-                    self.faces.pop(index)
-            for (index, vertex) in enumerate(self.vertices):
-                if vertex is None:
-                    self.vertices.pop(index)
+            new_nb_vert = len([i for i in self.vertices if i is not None])
+            # self.faces = [i for i in self.faces if i is not None]
+            # counter = 0
+            # l = []
+            # for f in self.faces:
+            #     if f.a == f.b:
+            #         counter += 1
+            #         l.append(f)
+            #     if f.a == f.c:
+            #         counter += 1
+            #         l.append(f)
+            #     if f.b == f.c:
+            #         counter += 1
+            #         l.append(f)
+            # if counter != 0:
+            #     print(l)
+            
+            
+            # print("Y a t il des None dans self.vertices ? ")
+            # print(any(x is None for x in self.vertices))
+            # print("Y a t il des None dans self.faces ? ")
+            # print(any(x is None for x in self.faces))
+            tour += 1
             
                     
 
@@ -53,9 +74,11 @@ class Test(obja.Model):
         
         
         op.reverse()
+        print(op)
         
         
         for (ty, index, value) in op:
+            print((ty, index, value))
             if ty == 'av':
                 output_model.add_vertex(index, value)
             elif ty == 'af':
@@ -74,6 +97,9 @@ class Test(obja.Model):
         un ordre de contour.
         (L'ordre se décide lors de l'appel de la fonction avec le paramètre vert_prec)
         """
+        # print('list_faces : '+str(list_faces))
+        # print('del_vert : '+str(del_vert))
+        # print('vert_prec : '+str(vert_prec))
         if list_faces == []:
             return []
         else:
@@ -82,6 +108,8 @@ class Test(obja.Model):
                 (f,_) = list_faces[ind]
                 # liste des sommets de la facette courante
                 verts_f = [f.a, f.b, f.c]
+                # print(verts_f)
+                # print('--------------------------------------')
                 # si le sommet à supprimer fait parti de la facette courante
                 # et le sommet précédent fait aussi parti de la facette courante
                 if del_vert in verts_f and vert_prec in verts_f:
@@ -89,10 +117,13 @@ class Test(obja.Model):
                     for v in verts_f:
                         # Si le sommet courant n'est pas le sommet à supprimer ni le sommet précédent
                         if v != del_vert and v != vert_prec:
+                            self.faces_rec.append(list_faces[ind])
                             # Suppression de la facette courante
                             list_faces.pop(ind)
                             # Ajout du sommet courant dans le liste des sommets du patch
-                            return [v] + self.sort_patch_rec(list_faces, del_vert, v)
+                            zouz = self.sort_patch_rec(list_faces, del_vert, v)
+                            # print(zouz)
+                            return [v] + zouz
                     
     def find_patches(self, vertices_to_delete):
         """
@@ -116,14 +147,17 @@ class Test(obja.Model):
         # ----------- TODO : check avec Kuremon pour avoir des index
         for vert_index in vertices_to_delete:   
             
+            self.faces_rec.clear()
+            
             # Iterate through the faces and store them temporarily
             for i in range(len(self.faces)):
                 face = self.faces[i]
-                if vert_index in [face.a, face.b, face.c]:
-                    # Ajout de la facette courante dans la liste intermédiaire
-                    temp_faces_list.append((face,i))
+                if face is not None:
+                    if vert_index in [face.a, face.b, face.c]:
+                        # Ajout de la facette courante dans la liste intermédiaire
+                        temp_faces_list.append((face,i))
             
-            faces_in_the_patches.append(list(temp_faces_list))
+            # faces_in_the_patches.append(list(temp_faces_list))
 
 
             if vert_index == temp_faces_list[0][0].a:
@@ -138,11 +172,15 @@ class Test(obja.Model):
                 next_vertex = temp_faces_list[0][0].b
 
             # construit la liste des sommets du patch
+            # print(temp_faces_list)
+            # print('########################################')
             patch = self.sort_patch_rec(temp_faces_list, vert_index, next_vertex)
+            # print('########################################')
 
             # ajoute la liste des sommets du patch à la liste des patchs
             patches.append(patch)
-        
+            faces_in_the_patches.append(list(self.faces_rec))
+                
         return (patches, faces_in_the_patches)
 
 
@@ -160,38 +198,87 @@ def trace_Z(self, patches, faces_in_the_patches, vertices_to_delete, operations)
     for i in range(len(patches)):
         patch = patches[i]
         #supprimer deux faces en face l'une de l'autre
-        n2 = math.floor(len(faces_in_the_patches[i])/2)
+        n2 = math.ceil(len(faces_in_the_patches[i])/2)
         operations.append(('af', 0, faces_in_the_patches[i][0][0]))
         ind_to_del = self.faces.index(faces_in_the_patches[i][0][0])
         self.faces[ind_to_del] = None
         operations.append(('af', n2, faces_in_the_patches[i][n2][0]))
         ind_to_del = self.faces.index(faces_in_the_patches[i][n2][0])
         self.faces[ind_to_del] = None
-        vert_del = vertices_to_delete[i]
+        vert_del = vertices_to_delete[i]        
+        
+        # print('taille du patch : '+str(len(faces_in_the_patches[i])))
+        # print('n2 = '+str(n2))
+        # print('patch : ')
+        # print(faces_in_the_patches[i])
+        # print()
+        # print(patches[i])
+        # print()
+        # print(vertices_to_delete)
+        # print()
+
         for j in range(len(faces_in_the_patches[i])):
             (face, index) = faces_in_the_patches[i][j]
-            if self.faces[index] != None:
+            if self.faces[index] is not None:                   ########## ICI J'AI CHANGE != PAR is not
                 # modifier les n-2 faces restantes
                 if vert_del == face.a:
-                    if face.b == 0 | face.b == n2:
+                    
+                    if patch.index(face.b) == 0 :
+                        new_face = obja.Face(face.b, face.c, patch[-patch.index(face.c)])
+                    elif patch.index(face.b) == n2:
                         new_face = obja.Face(face.b, face.c, patch[-patch.index(face.c)])
                     else:
                         new_face = obja.Face(face.b, face.c, patch[-patch.index(face.b)])
+                        
                 elif vert_del == face.b:
-                    if face.c == 0 | face.c == n2:
+                    
+                    if patch.index(face.c) == 0 :
+                        new_face = obja.Face(face.c, face.a, patch[-patch.index(face.a)])
+                    elif patch.index(face.c) == n2:
                         new_face = obja.Face(face.c, face.a, patch[-patch.index(face.a)])
                     else:
-                        new_face = obja.Face(face.c, face.a, patch[-patch.index(face.c)])  
+                        new_face = obja.Face(face.c, face.a, patch[-patch.index(face.c)])
+                        
                 elif vert_del == face.c:
-                    if face.a == 0 | face.a == n2:
+                    
+                    if patch.index(face.a) == 0 :
+                        new_face = obja.Face(face.a, face.b, patch[-patch.index(face.b)])
+                    elif patch.index(face.a) == n2:
                         new_face = obja.Face(face.a, face.b, patch[-patch.index(face.b)])
                     else:
                         new_face = obja.Face(face.a, face.b, patch[-patch.index(face.a)])
+                        
                 else:
                     print("ERREUR !!")
+                    print(vert_del)
                     
                 operations.append(('ef', index, face))
                 self.faces[index] = new_face
+                            
+                # if new_face.a == new_face.b:
+                #     print('indice de la face dans le patch : '+str(j))
+                #     print('=========================================')
+                #     print((face, index))
+                #     print('->')
+                #     print(new_face)
+                #     print('-----------------------------')
+                # elif new_face.b == new_face.c:
+                #     print('indice de la face dans le patch : '+str(j))
+                #     print('=========================================')
+                #     print((face, index))
+                #     print('->')
+                #     print(new_face)
+                #     print('-----------------------------')
+                # elif new_face.c == new_face.a:
+                #     print('indice de la face dans le patch : '+str(j))
+                #     print('=========================================')
+                #     print((face, index))
+                #     print('->')
+                #     print(new_face)
+                #     print('-----------------------------')
+                
+                    
+                
 
             
         # supprimer le sommet du milieu du patch        
@@ -220,63 +307,65 @@ def decimation(self):
         # Parcours de l'ensemble des sommets par indice (vertex_index) et valeur du sommet (vertex)
         for (vertex_index, vertex) in enumerate(self.vertices):
 
-            # Définition de la liste des points formant le patch 
-            liste_points = []
+            if vertex is not None:
+                # Définition de la liste des points formant le patch 
+                liste_points = []
 
-            # Parcours de l'ensemble des facettes par indice (face_index) et valeur de la facette (face)
-            for (face_index, face) in enumerate(self.faces):
+                # Parcours de l'ensemble des facettes par indice (face_index) et valeur de la facette (face)
+                for (face_index, face) in enumerate(self.faces):
 
-                # Liste des points d'une facette
-                L = [face.a, face.b, face.c]
+                    if face is not None:
+                        # Liste des points d'une facette
+                        L = [face.a, face.b, face.c]
 
-                # Si le sommet courant fait parti de la facette
-                if vertex_index in L:
+                        # Si le sommet courant fait parti de la facette
+                        if vertex_index in L:
 
-                    # Parcours des sommets d'une facette
-                    for l in L:
+                            # Parcours des sommets d'une facette
+                            for l in L:
 
-                        # Si le sommet courant n'appartient pas à la liste des points d'un patch 
-                        if l not in liste_points+[vertex_index]:
-                            # on ajoute le point à la liste de points d'un patch
-                            liste_points.append(l)
-                        # fin sommet courant n'appartient pas dans les points d'un patch
-                    # fin parcours des sommets d'une facette
-                # fin de sommet courant fait parti de la facette
+                                # Si le sommet courant n'appartient pas à la liste des points d'un patch 
+                                if l not in liste_points+[vertex_index]:
+                                    # on ajoute le point à la liste de points d'un patch
+                                    liste_points.append(l)
+                                # fin sommet courant n'appartient pas dans les points d'un patch
+                            # fin parcours des sommets d'une facette
+                    # fin de sommet courant fait parti de la facette
 
-            # Si le nombre de points d'un patch est supérieur ou égal à 5 (on ne travaille pas sur des patchs de taille inférieure
-            # à 5)
-            if len(liste_points) >= 5:
+                # Si le nombre de points d'un patch est supérieur ou égal à 5 (on ne travaille pas sur des patchs de taille inférieure
+                # à 5)
+                if len(liste_points) >= 5:
 
-                # Calcul des coefficients du plan moyen pour ce patch
-                A = np.ones((len(liste_points)+1, 4))
-                # Première ligne initialisée avec le sommet courant
-                A[0, 0:3] = vertex
-                print('liste des vertices du mesh : '+str(self.vertices))
-                # Parcours des points du patch par indice (i) et valeur (l)
-                for (i, l) in enumerate(liste_points):
-                    # i-ème ligne initialisée avec les sommets du patch
-                    A[i+1, 0:3] = self.vertices[l]
-                # fin parcours des points du patch
-                tAA = A.transpose() @ A
-                print("tAA = "+str(tAA))
+                    # Calcul des coefficients du plan moyen pour ce patch
+                    A = np.ones((len(liste_points)+1, 4))
+                    # Première ligne initialisée avec le sommet courant
+                    A[0, 0:3] = vertex
+                    # print('liste des vertices du mesh : '+str(self.vertices))
+                    # Parcours des points du patch par indice (i) et valeur (l)
+                    for (i, l) in enumerate(liste_points):
+                        # i-ème ligne initialisée avec les sommets du patch
+                        A[i+1, 0:3] = self.vertices[l]
+                    # fin parcours des points du patch
+                    tAA = A.transpose() @ A
+                    # print("tAA = "+str(tAA))
 
-                # Calcul des vecteurs propres et valeurs prorpes 
-                valeurs_propres, vecteurs_propres = np.linalg.eig(tAA)
-                # Indice de la plus petite valeur propre
-                ind = np.argmin(valeurs_propres)
-                # Vecteur propre associé à la plus petite valeur propre
-                X = vecteurs_propres[:, ind]
-                # Calcul de
-                n = np.array(X[0:3])
-                # Calcul de la distance entre le sommet courant (vertex) et le plan moyen
-                k = (- X[3] - n.transpose() @ vertex) / np.linalg.norm(n)
-                # Mémorise la distance du sommet courant et du plan moyen
-                distances.append(abs(k))
-                # Mémorise l'indice du sommet courant
-                index_vertices.append(vertex_index)
-                # Mémorise les voisins du somme courant
-                voisins_vertices.append(liste_points[:])
-            # fin len(liste_points) >= 5
+                    # Calcul des vecteurs propres et valeurs prorpes 
+                    valeurs_propres, vecteurs_propres = np.linalg.eig(tAA)
+                    # Indice de la plus petite valeur propre
+                    ind = np.argmin(valeurs_propres)
+                    # Vecteur propre associé à la plus petite valeur propre
+                    X = vecteurs_propres[:, ind]
+                    # Calcul de
+                    n = np.array(X[0:3])
+                    # Calcul de la distance entre le sommet courant (vertex) et le plan moyen
+                    k = (- X[3] - n.transpose() @ vertex) / np.linalg.norm(n)
+                    # Mémorise la distance du sommet courant et du plan moyen
+                    distances.append(abs(k))
+                    # Mémorise l'indice du sommet courant
+                    index_vertices.append(vertex_index)
+                    # Mémorise les voisins du somme courant
+                    voisins_vertices.append(liste_points[:])
+                # fin len(liste_points) >= 5
         # fin du parcours des sommets
 
 
